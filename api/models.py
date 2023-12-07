92ef0f21713bca99f7b559caae429802957ae1df
Mursito1 <128065108+Mursito1@users.noreply.github.com>
2023-12-06 22:36:56 -0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -4,10 +4,12 @@ from django.db import models
 
 class Profesor(models.Model):
     username = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     is_active = models.BooleanField(default=True)
 
 class Alumno(models.Model):
     username = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     is_active = models.BooleanField(default=True)
